797e3c6eec8a6f599ac2f93f77de1ebf4803ce81
binarnesia <binarys.targaryen@gmail.com>
2023-08-03 11:48:23 +0800

    add app constants value

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF757EFA);
+  static const Color secondaryColor = Color(0xFF66D0A4);
+  static const Color primaryAccent = Color(0xFFB0B6FF);
+  static const Color white = Colors.white;
+  static const Color danger = Color(0xFFEF6497);
+}
