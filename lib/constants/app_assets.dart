778a9ffda58fba3948c8349638a167f1085c86e6
binarnesia <binarys.targaryen@gmail.com>
2023-08-03 11:57:51 +0800

    change image to svg

--- lib/constants/app_assets.dart
+++ lib/constants/app_assets.dart
@@ -1,7 +1,7 @@
 class AppAssets {
   static const String bg = 'assets/bg.png';
-  static const String circle = 'assets/circle.png';
-  static const String cross = 'assets/cross.png';
+  static const String circle = 'assets/circle.svg';
+  static const String cross = 'assets/cross.svg';
   static const String iconSplash = 'assets/icon_splash.png';
   static const String icon = 'assets/icon.png';
 }
